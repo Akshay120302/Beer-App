e60fac82dacc70afee41820f8b22a1d23a715ab0
Akshay Sinha <108929978+Akshay120302@users.noreply.github.com>
2023-09-21 16:35:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BeerList from './BeerList';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <BeerList />
+    </div>
+  );
+}
+
+export default App;
